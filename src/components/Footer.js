16e171177b633ee736bcb6355f569b368c7b5bdf
Evans Acheampong <evansachie01@gmail.com>
2024-03-17 14:47:37 +0000

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>Made by <a href="https://www.linkedin.com/in/evans-acheampong/" target="_blank" rel="noopener noreferrer">Evans Acheampong</a></p>
+        </footer>
+    );
+}
