fbfba2da210433a6b6fd14e3126413a74b30b54d
Evans Acheampong <evansachie01@gmail.com>
2024-03-17 16:01:13 +0000

    Added footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <footer className="footer">
-            <p>Made by <a href="https://www.linkedin.com/in/evans-acheampong/" target="_blank" rel="noopener noreferrer">Evans Acheampong</a></p>
+            <p>Made by: <a href="https://www.linkedin.com/in/evans-acheampong/" target="_blank" rel="noopener noreferrer">Evans Acheampong</a></p>
             <a href="https://github.com/AWESOME04/meme-generator" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
         </footer>
     );
