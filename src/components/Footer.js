37df4590cefb7427bd8e5ffc6147cea5255e30ce
Evans Acheampong <evansachie01@gmail.com>
2024-03-17 15:05:45 +0000

    Added footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <p>Made by <a href="https://www.linkedin.com/in/evans-acheampong/" target="_blank" rel="noopener noreferrer">Evans Acheampong</a></p>
+            <a href="https://github.com/AWESOME04/meme-generator" target="_blank" rel="noopener noreferrer">GitHub Repository</a>
         </footer>
     );
 }
